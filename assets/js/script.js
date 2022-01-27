d13eb452b5e6130901f8ecda4627e348e199f3cd
TimRas <martinusras95@gmail.com>
2022-01-27 19:17:15 +0000

    load json file and show random question

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,13 +19,20 @@ var myGamerName = document.getElementById("gamer-name");
 
 var myGameBody = document.getElementById("game-body");
 
+// Gets main question div
+var myMainQuestion = document.getElementById("main-question");
+
+// Gets answer buttons
+var firstAnswer = document.getElementById("first-answer");
+var secondAnswer = document.getElementById("second-answer");
+var thirdAnswer = document.getElementById("third-answer");
+
 // Nieuwe Let aanmaken
 let nameFieldValue = "";
 
-const questionBank = '[{ "question": "Who was the person to travel to space?","answers": { "a": "Yuri Alekseyevich Gagarin", "b": "Neil Armstrong","c": "William E. Thornton"},"correctAnswer": "Yuri Alekseyevich Gagarin"},{"question": "What was the first animal to travel to space?","answers": { "a": "Mika the monkey","b": "Olaf the chimp","c": "Laika the dog"},"correctAnswer": "Laika the dog"}]';
+var questionLibrary;
 
-// Parse library to array
-const questionLibrary = JSON.parse(questionBank);
+
 
 // Enters name value in gamer-name div when pressed start
 confirmNameButton.onclick = function () {
@@ -39,8 +46,28 @@ function setGamerName() {
 
 function getNewQuestion() {
    var randomQuestionIndex = randomIntBetween(0,questionLibrary.length - 1);
-   var question = questionLibrary[randomQuestionIndex];
+   console.log(questionLibrary.length);
+   var questionObject = questionLibrary[randomQuestionIndex];
+   console.log(questionObject);
+   myMainQuestion.innerHTML = questionObject.question;
+   firstAnswer.innerHTML = questionObject.answers.a;
+   secondAnswer.innerHTML = questionObject.answers.b;
+   thirdAnswer.innerHTML = questionObject.answers.c;
 }
+
+function parseQuestionBank() {
+    fetch("assets/content/questionbank.JSON").then( storageFile =>
+        {
+            storageFile.json().then( questionArray => {
+                questionLibrary = questionArray;
+                getNewQuestion();   
+            });
+            
+
+        }
+    );
+    
+} 
 
 function randomIntBetween(min,max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
@@ -86,124 +113,3 @@ nameField.onkeydown = function () {
 
 //question bank for the quiz
 
-// const questionBank = '[{ "question": "Who was the person to travel to space?","answers": { "a": "Yuri Alekseyevich Gagarin", "b": "Neil Armstrong","c": "William E. Thornton"},"correctAnswer": "Yuri Alekseyevich Gagarin"},{"question": "What was the first animal to travel to space?","answers": { "a": "Mika the monkey","b": "Olaf the chimp","c": "Laika the dog"},"correctAnswer": "Laika the dog"}]';
-
-//     question: "What is the biggest planet in our solar system?",
-//     answers: {
-//         a: "Saturn",
-//         b: "Uranus",
-//         c: "Jupiter"
-//     },
-//     correctAnswer: "Jupiter"
-// }, {
-//     question: "How long does the earth takes to complete its rotation around the sun?",
-//     answers: {
-//         a: "24 hours",
-//         b: "365 days",
-//         c: "15 months"
-//     },
-//     correctAnswer: "365 days"
-// }, {
-//     question: "How many people have been to the moon?",
-//     answers: {
-//         a: "12 people",
-//         b: "24 people",
-//         c: "7 people"
-//     },
-//     correctAnswer: "12 people"
-// }, {
-//     question: "What is the smallest planet in our solar system?",
-//     answers: {
-//         a: "Mars",
-//         b: "Earth",
-//         c: "Mercury"
-//     },
-//     correctAnswer: "Mercury"
-// }, {
-//     question: "How many miles do you need to go upwards to reach space?",
-//     answers: {
-//         a: "34 miles",
-//         b: "62 miles",
-//         c: "103 miles"
-//     },
-//     correctAnswer: "62 miles"
-// }, {
-//     question: "Which country was to first to reach space?",
-//     answers: {
-//         a: "Soviet Union",
-//         b: "United States",
-//         c: "China"
-//     },
-//     correctAnswer: "Soviet Union"
-// }, {
-//     question: "which of these planets is a gas planet?",
-//     answers: {
-//         a: "Mars",
-//         b: "Saturn",
-//         c: "Venus"
-//     },
-//     correctAnswer: "Saturn"
-// }, {
-//     question: "How many moons does Jupiter have?",
-//     answers: {
-//         a: "79 moons",
-//         b: "4 moons",
-//         c: "12 moons"
-//     },
-//     correctAnswer: "79 moons"
-// }, {
-//     question: "Which planet orbits the Sun the fastest?",
-//     answers: {
-//         a: "Mars",
-//         b: "Earth",
-//         c: "Mercury"
-//     },
-//     correctAnswer: "Mercury"
-// }, {
-//     question: "Which planet has the least amount of moons orbiting it?",
-//     answers: {
-//         a: "Earth",
-//         b: "Venus",
-//         c: "Jupiter"
-//     },
-//     correctAnswer: "venus"
-// }, {
-//     question: "Which planet is closest to the Sun?",
-//     answers: {
-//         a: "Venus",
-//         b: "Mars",
-//         c: "Mercury"
-//     },
-//     correctAnswer: "Mercury"
-// }, {
-//     question: "What's the oldest planet in our solar system?",
-//     answers: {
-//         a: "Mercury",
-//         b: "Jupiter",
-//         c: "Earth"
-//     },
-//     correctAnswer: "Jupiter"
-// }, {
-//     question: "How many miles are there between the Earth and the Sun?",
-//     answers: {
-//         a: "93 million miles",
-//         b: "48 million miles",
-//         c: "142 million miles"
-//     },
-//     correctAnswer: "93 million miles"
-// }, {
-//     question: "How many females have been to space?",
-//     answers: {
-//         a: "32 women",
-//         b: "65 women",
-//         c: "12 women"
-//     },
-//     correctAnswer: "65 women"
-// }, {
-
-// }
-
-
-
-// ]
-// ';
