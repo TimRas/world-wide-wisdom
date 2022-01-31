873749cc0a565c4204036c001807937a30c95fc3
TimRas <martinusras95@gmail.com>
2022-01-31 04:37:59 +0000

    add comments in js script, rename some variables and elements, clean the pages and adjust some css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,56 @@
-// Get the modal
-var myModalInstruction = document.getElementById("myModalInstruction");
-var myModalEnterName = document.getElementById("myModalEnterName");
+//Gets the modal
+var modalInstructions = document.getElementById("modal-instructions");
+var modalEnterName = document.getElementById("modal-enter-name");
 
-// Get the button that opens the modal
+//Gets the button that opens the modal
 var startButton = document.getElementById("button-div");
 var nextButton = document.getElementById("next-div");
-var confirmNameButton = document.getElementById("confirm-button-div");
+var confirmNameButton = document.getElementById("begin-button-div");
 
-// Get the <span> element that closes the modal
-var spanInstruction = document.getElementById("closeInstruction");
+//Gets the <span> element that closes the modal
+var spanInstruction = document.getElementById("close-instruction");
 var spanEnterName = document.getElementById("close-enter-name");
 
-// Get the namefield that opens the modal
-var nameField = document.getElementById("namefieldid");
+//Gets the namefield that opens the modal
+var nameField = document.getElementById("name-field-id");
 
-// Gets gamer-name div
-var myGamerNameElement = document.getElementById("gamer-name");
+//Gets gamer-name div
+var gamerNameElement = document.getElementById("gamer-name");
 
 //Gets counter div
 var questionCounter = document.getElementById("questions-counter");
 
-// Gets main question div
-var myMainQuestion = document.getElementById("main-question");
+//Gets main question div
+var mainQuestion = document.getElementById("main-question");
 
 //Gets highscore body
-var myHighscoreBody = document.getElementById("highscore-body");
+var highScoreBody = document.getElementById("highscore-body");
 
-// Gets answer buttons
+//Gets answer buttons
 var firstAnswer = document.getElementById("first-answer");
 var secondAnswer = document.getElementById("second-answer");
 var thirdAnswer = document.getElementById("third-answer");
 
 //Tracks already shown questions
-
 var shownQuestions = [];
 
-// Tracks current question
+//Tracks current question
 var currentQuestion;
 
-// Nieuwe Let aanmaken
-let nameFieldValue = "";
+var nameFieldValue = "";
 
+//Array of questions
 var questionLibrary;
 
 //Tracks total amount of questions
-
 var totalQuestionTracker = 1;
 
 //Tracks score
-
 var totalScoreTracker = 0;
 
-if (myHighscoreBody != undefined) {
-    myHighscoreBody.addEventListener('load',(event) => {
+
+if (highScoreBody != undefined) {
+    highScoreBody.addEventListener('load',(event) => {
         loadHighscores();
     } )
 }
@@ -61,11 +59,12 @@ if (myHighscoreBody != undefined) {
 confirmNameButton.onclick = function () {
     localStorage.setItem("gamername", nameField.value);
 }
-// Puts value of confirm-button-div is gamer-name div
+// Puts value of begin-button-div is gamer-name div
 function setGamerName() {
-    myGamerNameElement.innerHTML = localStorage.getItem("gamername");
+    gamerNameElement.innerHTML = localStorage.getItem("gamername");
 }
 
+//Gets new question
 function getNewQuestion() {
     if (totalQuestionTracker === 11) {
         localStorage.setItem("score", totalScoreTracker);
@@ -78,7 +77,7 @@ function getNewQuestion() {
         shownQuestions.push(randomQuestionIndex);
         currentQuestion = randomQuestionIndex;
         var questionObject = questionLibrary[randomQuestionIndex];
-        myMainQuestion.innerHTML = questionObject.question;
+        mainQuestion.innerHTML = questionObject.question;
         firstAnswer.innerHTML = questionObject.answers.a;
         secondAnswer.innerHTML = questionObject.answers.b;
         thirdAnswer.innerHTML = questionObject.answers.c;
@@ -87,6 +86,7 @@ function getNewQuestion() {
     }
 }
 
+//Parses questionbank into a array
 function parseQuestionBank() {
     fetch("assets/content/questionbank.JSON").then(storageFile => {
         storageFile.json().then(questionArray => {
@@ -99,10 +99,12 @@ function parseQuestionBank() {
 
 }
 
+//Creates random number for a to choose a question
 function randomIntBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+//Checks for right answer and starts next question
 function activateButtons() {
     firstAnswer.onclick = function () {
         var question = questionLibrary[currentQuestion];
@@ -136,28 +138,23 @@ function activateButtons() {
     };
 }
 
+//Checks, loads and saves highscores
 function loadHighscores() {
-    console.log("hallo ah mattie");
     var gamerName = localStorage.getItem("gamername");
     var highScores = JSON.parse(localStorage.getItem("highscores"));
     var gamerScore = localStorage.getItem("score");
-    console.log(highScores);
     var highScoreEntry = {
         "name": gamerName,
         "score": gamerScore
     };
     if (highScores === undefined || highScores === null) {
         highScores = [highScoreEntry];
-        console.log(highScores);
     } else {
         if (highScores.length < 10) {
             highScores.push(highScoreEntry);
-            console.log(highScores);
         } else {
-            // als score hoger is dan de score van nummer 10 voeg toe
             if (gamerScore > highScores[9].score) {
                 highScores.pop();
-                console.log(highScores);
                 highScores.push(highScoreEntry);
             }
         }
@@ -166,7 +163,6 @@ function loadHighscores() {
         return b.score - a.score;
     });
     localStorage.setItem("highscores", JSON.stringify(highScores));
-    console.log(highScores);
     for (let step = 0; step < highScores.length; step++) {
         var nameLabel = document.getElementById("name-wrap"+(step+1));
         var scoreWrap = document.getElementById("score-wrap"+(step+1));
@@ -175,32 +171,31 @@ function loadHighscores() {
       }
 }
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the button it opens the modal
 startButton.onclick = function () {
     if (nameFieldValue != '') {
-        //start de game.. 
     } else {
-        myModalInstruction.style.display = "block";
+        modalInstructions.style.display = "block";
     }
 }
 
 nextButton.onclick = function () {
-    myModalInstruction.style.display = "none";
-    myModalEnterName.style.display = "block";
+    modalInstructions.style.display = "none";
+    modalEnterName.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x) it closes the modal
 spanInstruction.onclick = function () {
-    myModalInstruction.style.display = "none";
+    modalInstructions.style.display = "none";
 }
 
 spanEnterName.onclick = function () {
-    myModalEnterName.style.display = "none";
+    modalEnterName.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
-        myModalInstruction.style.display = "none";
+        modalInstructions.style.display = "none";
     }
 }
